aacf464d66882e9fef21967e2f26c09d8e003a9d
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-11 16:07:43 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo_Variables
+{
+    internal class Program
+    {
+        static string program_name = "Démonstration pour les Games!";
+        static void Main(string[] args)
+        {
+            string message = "Hello world!";
+            Console.WriteLine(message);
+            Console.WriteLine(program_name);
+        }
+    }
+}
