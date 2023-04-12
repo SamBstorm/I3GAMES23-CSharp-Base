1708caabfcb7cdc7e4fbd32ecd0e039678c24ac1
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-12 15:04:14 +0200

    Démonstration : Conditionnel Partie 2

--- Demo-Conditionnel/Program.cs
+++ Demo-Conditionnel/Program.cs
@@ -14,13 +14,32 @@
             {
                 Console.WriteLine("Quel est votre nationalité ?");
                 string? nationalite = Console.ReadLine();
-                if(nationalite.ToUpper() == "Belge".ToUpper())
+                /*if(nationalite.ToUpper() == "Belge".ToUpper())
+                //{
+                //    Console.WriteLine("Bienvenu sur notre site concours!");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"Votre nationalité : '{nationalite}' n'est pas prise en charge...");
+                //}*/
+
+                switch(nationalite.ToLower())
                 {
-                    Console.WriteLine("Bienvenu sur notre site concours!");
-                }
-                else
-                {
-                    Console.WriteLine($"Votre nationalité : '{nationalite}' n'est pas prise en charge...");
+                    case "belge":
+                        Console.WriteLine("Bienvenu sur notre site une fois!");
+                        break;
+                    case "français":
+                        Console.WriteLine("Cocorico! Bienvenu!");
+                        break;
+                    case "québequois":
+                        Console.WriteLine("Monte dans l'char! Tabernacle!");
+                        break;
+                    case "suisse":
+                        Console.WriteLine("Bienvveennnnuuueee!");
+                        break;
+                    default:
+                        Console.WriteLine($"Votre nationalité : '{nationalite}' n'est pas prise en charge...");
+                        break;
                 }
             }
             else
