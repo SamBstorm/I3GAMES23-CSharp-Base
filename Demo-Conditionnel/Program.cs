c9689eaf64bf78cbb75a13c9c8b165ee503a1137
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-12 14:39:48 +0200

    Démonstration : Conditionnel Partie 1

---
+++ Demo-Conditionnel/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Demo_Conditionnel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quel est votre age ?");
+            string? age_str = Console.ReadLine();
+
+            int age;
+            bool age_isConvert = int.TryParse(age_str, out age);
+
+            if(age_isConvert == true && age >= 18)
+            {
+                Console.WriteLine("Quel est votre nationalité ?");
+                string? nationalite = Console.ReadLine();
+                if(nationalite.ToUpper() == "Belge".ToUpper())
+                {
+                    Console.WriteLine("Bienvenu sur notre site concours!");
+                }
+                else
+                {
+                    Console.WriteLine($"Votre nationalité : '{nationalite}' n'est pas prise en charge...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Reviens quand tu auras l'âge...");
+            }
+        }
+    }
+}
