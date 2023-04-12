8b5abb52646e44396b8a7e39d708692b8b830d2f
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-12 13:55:39 +0200

    Ajout Exercice 2 : FIN

--- Exo02/Program.cs
+++ Exo02/Program.cs
@@ -7,7 +7,7 @@
             /*
              Exercice 2 : slide 104
              */
-            /* Version 1 : Version décomposée
+            /* Version Parse(...) : Version décomposée
             Console.WriteLine("Introduire un premier nombre :");
             string? nb1_str = Console.ReadLine();
 
@@ -23,7 +23,7 @@
             Console.WriteLine($"{nb1} + {nb2} = {sum}");
             */
 
-            /* Version 1 : Version simplifié 
+            /* Version Parse(...) : Version simplifié 
              
             Console.WriteLine("Introduire un premier nombre:");
             int nb1 = int.Parse(Console.ReadLine());
@@ -33,6 +33,34 @@
 
             Console.WriteLine($"{nb1} + {nb2} = {nb1 + nb2}");
             */
+
+            /* Version TryParse(...) : Version décomposée
+            Console.WriteLine("Entrer un nombre :");
+            string nb1_str = Console.ReadLine();
+
+            int nb1;
+            bool nb1_isConvert = int.TryParse(nb1_str, out nb1);
+
+            Console.WriteLine("Entrer un nombre :");
+            string nb2_str = Console.ReadLine();
+
+            int nb2;
+            bool nb2_isConvert = int.TryParse(nb2_str, out nb2);
+
+            Console.WriteLine($"La conversion du premier nombre : {nb1_isConvert}");
+            Console.WriteLine($"La conversion du second nombre : {nb2_isConvert}");
+            Console.WriteLine($"L'addition des 2 nombres est : {nb1 + nb2}");
+            */
+
+            /* Version TryParse(...) : Version simplifié*/
+
+            Console.WriteLine("Entrer un nombre :");
+            int.TryParse(Console.ReadLine(), out int nb1);
+
+            Console.WriteLine("Entrer un nombre :");
+            int.TryParse(Console.ReadLine(), out int nb2);
+
+            Console.WriteLine($"L'addition des 2 nombres est : {nb1 + nb2}");
         }
     }
 }
