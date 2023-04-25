6e1b731d2b4cfaa75c75eff37e2b78771c09c13f
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-25 14:41:25 +0200

    Ajout Demo-Substring

---
+++ Demo-Substring/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello, World!";
+
+            //"H e l l o , _ W o r l d "
+            // 0 1 2 3 4 5 6 7 8 9 1011
+
+            Console.WriteLine(message);
+            Console.WriteLine(message.Substring(0,5));
+            Console.WriteLine(message.Substring(7,5));
+        }
+    }
+}
