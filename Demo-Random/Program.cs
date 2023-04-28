0130a6d0aef06fb741b87c3150f678066dd45696
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-28 11:45:35 +0200

    Ajout Correction Exo10-PlusOuMoins

--- Demo-Random/Program.cs
+++ Demo-Random/Program.cs
@@ -9,7 +9,7 @@
             for (int i = 0; i < 10; i++)
             {
                 int nb1 = RNG.Next();      //Entre 0 et le MAX d'un int32 (2.147.483.647)
-                int nb2 = RNG.Next(11);    //Entre 0 et la valeur indiquée non-inclus (de 0 à 10) 
+                short nb2 = (short)RNG.Next(11);    //Entre 0 et la valeur indiquée non-inclus (de 0 à 10) 
                 int nb3 = RNG.Next(5,11);  //Entre la première et la seconde valeur non-inclus (de 5 à 10)
                 Console.WriteLine(nb1);
             }
---
+++ Exo10-PlusOuMoins01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exo10_PlusOuMoins01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenue au jeu du Juste Prix!");
+            Console.WriteLine("Veuillez indiquer le Juste prix à l'abris des regards :");
+            ushort juste_prix = ushort.Parse(Console.ReadLine());
+            ushort proposition;
+            Console.Clear();
+            string message = "Joueur! Quel est selon vous le Juste Prix?";
+            do
+            {
+                Console.WriteLine(message);
+                proposition = ushort.Parse(Console.ReadLine());
+                if (proposition > juste_prix)
+                {
+                    message = "C'est moins! (nouvelle proposition)";
+                }
+                else if (proposition < juste_prix)
+                {
+                    message = "C'est plus! (nouvelle proposition)";
+                } 
+            } while (proposition != juste_prix);
+
+            Console.WriteLine("Bravo! Vous avez deviné le Juste Prix!");
+        }
+    }
+}
