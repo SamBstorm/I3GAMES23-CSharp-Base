8660b1ecb9add394a9b0a0a5b1b5e1a21f48c6f1
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-28 11:01:02 +0200

    Ajout Démonstration Random

---
+++ Demo-Random/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo_Random
+{
+    internal class Program
+    {
+        static Random RNG = new Random();   //ATTENTION!!! La déclaration de RNG se fait dan la class, pas dans le Main()
+                                            //ATTENTION!!! La variable doit être static
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int nb1 = RNG.Next();      //Entre 0 et le MAX d'un int32 (2.147.483.647)
+                int nb2 = RNG.Next(11);    //Entre 0 et la valeur indiquée non-inclus (de 0 à 10) 
+                int nb3 = RNG.Next(5,11);  //Entre la première et la seconde valeur non-inclus (de 5 à 10)
+                Console.WriteLine(nb1);
+            }
+        }
+    }
+}
---
+++ Exo09-TableMultiplication/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exo09_TableMultiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int table = 1; table <= 9; table++)
+            {
+                Console.WriteLine($"Table de {table}");
+                for(int multi = 1; multi <= 10; multi++)
+                {
+                    int calcul = table * multi;
+                    Console.Write($"{table} x {multi} = {calcul} ; ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
