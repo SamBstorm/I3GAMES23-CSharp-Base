fdd938a8a28ae3ff7a648e3805e66c91deb8ee36
Samuel Legrain <samuel.legrain@bstorm.be>
2023-05-22 11:28:01 +0200

    Ajout : Exo Tableau 02

---
+++ ExoTab-02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExoTab_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAILLE = 10;
+            int[] tab = new int[TAILLE];
+            //      [2,4,8,0,0,0,0,0,0,0]
+            //index :0 1 2 3 4 5 6 7 8 9 
+
+            tab[0] = 2;
+            for (int i = 1;i < TAILLE; i++)
+            {
+                tab[i] = tab[i - 1] * 2;
+            }
+
+            foreach(int chiffre in tab)
+            {
+                Console.WriteLine(chiffre);
+            }
+            #region Comment fonctionne le foreach
+            /*
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int chiffre = tab[i];
+                Console.WriteLine(chiffre);
+            }
+            */
+            #endregion
+        }
+    }
+}
