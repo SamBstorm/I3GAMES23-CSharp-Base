eca4e0898476e4d4b4c40ce2e6941364fa2c3e91
Samuel Legrain <samuel.legrain@bstorm.be>
2023-05-22 14:20:58 +0200

    Ajout Exercice Tableau 4

---
+++ ExoTab-04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExoTab_04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Création du tableau
+            string[] students = { "Alexia", "Loriane", "Naël", "Sonia", "Tonia", "Audrey", "Cécile", "Carol", "Hafsa", "Feryel" };
+
+            //inversion du tableau
+            for (int i = 0; i < students.Length/2; i++)
+            {
+                string temp = students[i];
+                students[i] = students[students.Length - 1 - i];
+                students[students.Length - 1 - i] = temp;
+            }
+
+            //affichage du tableau
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+}
