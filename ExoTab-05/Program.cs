daf97ac1e4e43fa0f3352541e1c7ef30bbf30daf
Samuel Legrain <samuel.legrain@bstorm.be>
2023-05-22 15:06:06 +0200

    Ajout Exercice Tableau 5

---
+++ ExoTab-05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExoTab_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = { 5, 3, 8, 1, 123, 5, 9};
+
+            bool sorted = false;
+
+            for (int turn = 0; !sorted; turn++)
+            {
+                sorted = true;
+                for (int i = 0; i < tab.Length-1-turn; i++)
+                {
+                    if (tab[i] > tab[i+1])
+                    {
+                        int temp = tab[i];
+                        tab[i] = tab[i+1];
+                        tab[i+1] = temp;
+                        sorted = false;
+                    }
+                }
+            }
+
+            foreach (int number in tab)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
