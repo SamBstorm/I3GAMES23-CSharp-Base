e9ce32deefd7a457435f9ba6f7716e09723890f9
Samuel Legrain <samuel.legrain@bstorm.be>
2023-05-23 11:46:00 +0200

    Ajout Exercice Tableau 7 + Démo console avancée

---
+++ Demo-Pion/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Text;
+
+namespace Demo_Pion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialisation
+            Console.OutputEncoding = Encoding.UTF8;
+            const string PION = "😉";
+            const string VIDE = "-";
+            const int TAILLE = 10;
+
+            bool[] grid = new bool[TAILLE];
+
+            int position = 0;
+            grid[position] = true;
+
+            ConsoleKey direction;
+
+            //Jeu déplacement
+            do
+            {
+                //Affichage
+                Console.Clear();
+                foreach (bool cell in grid)
+                {
+                    Console.Write((cell) ? PION : VIDE);
+                }
+                Console.WriteLine();
+                //Demander direction
+                Console.WriteLine("Voulez-vous aller à (G)auche, à (D)roite ou (Q)uitter?");
+                direction = Console.ReadKey(true).Key;
+
+                //Gestion direction
+                switch (direction)
+                {
+                    case ConsoleKey.D:
+                        if (!grid[grid.Length - 1])
+                        {
+                            grid[position++] = false;
+                            grid[position] = true;
+                        }
+                        break;
+                    case ConsoleKey.G:
+                        if (!grid[0])
+                        {
+                            grid[position--] = false;
+                            grid[position] = true;
+                        }
+                        break;
+                }
+
+                /*
+                if (direction == "d" && !grid[grid.Length - 1])
+                {
+                    grid[position++] = false;
+                    grid[position] = true;
+                }
+                else if (direction == "g" && !grid[0])
+                {
+                    grid[position--] = false;
+                    grid[position] = true;
+                }*/
+
+            } while (direction != ConsoleKey.Q);
+
+            #region SANS tableau!
+            /*//Initialisation
+            Console.OutputEncoding = Encoding.UTF8;
+            const string PION = "😉";
+            const string VIDE = "-";
+            const int TAILLE = 10;
+
+            int position = 0;
+
+            string direction;
+
+            //Jeu déplacement
+            do
+            {
+                //Affichage
+                Console.Clear();
+                for (int i = 0; i < TAILLE; i++)
+                {
+                    Console.Write((i == position) ? PION : VIDE);
+                }
+                Console.WriteLine();
+                //Demander direction
+                Console.WriteLine("Voulez-vous aller à (G)auche, à (D)roite ou (Q)uitter?");
+                direction = Console.ReadLine().ToLower();
+
+                //Gestion direction
+                switch (direction)
+                {
+                    case "d":
+                        if (position != TAILLE - 1)
+                        {
+                            position++;
+                        }
+                        break;
+                    case "g":
+                        if (position != 0)
+                        {
+                            position--;
+                        }
+                        break;
+                }
+
+            } while (direction != "q");*/
+            #endregion
+        }
+    }
+}
---
+++ ExoTab-07/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace ExoTab_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INITIALISATION
+
+            Console.OutputEncoding = Encoding.UTF8;
+            const string PION = "😉";
+            const string VIDE = "-";
+            const int TAILLE = 10;
+            //string[] table = { "😉","-", "-", "-", "-", "-", "-", "-", "-", "-" };
+            string direction;
+
+            string[] table = new string[TAILLE];
+            table[0] = PION;
+            for (int i = 1; i < table.Length; i++)
+            {
+                table[i] = VIDE;
+            }
+            //Jeu en boucle
+            do
+            {
+                //Affichage table
+                Console.Clear();
+                foreach (string cell in table)
+                {
+                    Console.Write(cell);
+                }
+                Console.WriteLine();
+                //Demander direction
+                Console.WriteLine("Voulez-vous aller à (G)auche, à (D)roite ou (Q)uitter?");
+                direction = Console.ReadLine().ToLower();
+
+                //Récuperer Position
+                int position = -1;
+
+                for (int i = 0; i < table.Length && position == -1; i++)
+                {
+                    if (table[i] == PION)
+                    {
+                        position = i;
+                    }
+                }
+
+                //Vérifier direction
+                switch (direction)
+                {
+                    case "d":
+                        if (table[TAILLE - 1] != PION)
+                        {
+                            table[position] = VIDE;
+                            position++;
+                            table[position] = PION;
+                        }
+                        break;
+                    case "g":
+                        if (table[0] != PION)
+                        {
+                            table[position] = VIDE;
+                            position--;
+                            table[position] = PION;
+                        }
+                        break;
+                }
+            } while (direction != "q");
+        }
+    }
+}
